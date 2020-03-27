21d9f70e604fd2e49fd309a018382a76de4d0aa6
Zhaoyuan Huang <mgeorgetw@gmail.com>
2020-03-27 14:52:58 +0800

    Implemented react-grid-layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import "../node_modules/react-grid-layout/css/styles.css";
+import "../node_modules/react-resizable/css/styles.css";
 import * as Charts from "./Charts.js";
 import { Responsive, WidthProvider } from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
