f3dfeb219fd20e0bf20b011104ae504aa14c268c
Zhaoyuan Huang <mgeorgetw@gmail.com>
2021-06-05 18:56:53 +0800

    Updates

---
+++ precache-manifest.dfbfbe3dcf6952ff5c8de2cde163f7d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a43d797f66d22fd1c309761fa4495f",
+    "url": "/COVID-19-Dashboard/index.html"
+  },
+  {
+    "revision": "2b94897e0f3d818712ec",
+    "url": "/COVID-19-Dashboard/static/css/main.30a4fed3.chunk.css"
+  },
+  {
+    "revision": "cbbec25ae7094b6efeb8",
+    "url": "/COVID-19-Dashboard/static/js/2.31b43808.chunk.js"
+  },
+  {
+    "revision": "ea6f07640c638d2bd535f81537557097",
+    "url": "/COVID-19-Dashboard/static/js/2.31b43808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b94897e0f3d818712ec",
+    "url": "/COVID-19-Dashboard/static/js/main.4fad3887.chunk.js"
+  },
+  {
+    "revision": "48727a06f5368e41436d",
+    "url": "/COVID-19-Dashboard/static/js/runtime-main.50fe05e4.js"
+  },
+  {
+    "revision": "ad9e48a0b6f3c698e8979d6d9b51fa23",
+    "url": "/COVID-19-Dashboard/static/media/NotoSansMono-CondensedBold.ad9e48a0.ttf"
+  }
+]);
